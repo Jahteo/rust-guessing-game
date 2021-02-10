8c596cc34b7dcc9e47562fc894f29f685dd7c82b
Josh Glantz-Hucks <jahteo@gmail.com>
2021-02-09 21:19:13 -0800

    setup app, started writing input, lots of accidental debugging files that were auto-generated while playing with the debugger

---
+++ guessing_game/target/debug/deps/guessing_game-7323f8d857c6d42f.d
@@ -0,0 +1,5 @@
+/Users/skinnymac/Desktop/Lambda/CS2/rust-guessing-game/guessing_game/target/debug/deps/guessing_game-7323f8d857c6d42f.rmeta: src/main.rs
+
+/Users/skinnymac/Desktop/Lambda/CS2/rust-guessing-game/guessing_game/target/debug/deps/guessing_game-7323f8d857c6d42f.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/skinnymac/Desktop/Lambda/CS2/rust-guessing-game/guessing_game/target/debug/guessing_game: /Users/skinnymac/Desktop/Lambda/CS2/rust-guessing-game/guessing_game/src/main.rs
